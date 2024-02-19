b5cf0b041e61f038742d0bb51b0ec7bf6bed9200
isokkur <159803960+isokkur@users.noreply.github.com>
2024-02-19 13:12:46 +0700

    Робот

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassPotok;
+
+namespace PotokConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot("Robot");
+            for (int i = 0; i < 10; i++)
+            {
+                robot.Move();
+                robot.GetInfo();
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/code.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ClassPotok
+{
+    public class Robot
+    {
+        private int x { get; set; }
+        private int y { get; set; }
+        private string name { get; set; }
+        private Random random;
+
+        public Robot(string name)
+        {
+            this.name = name;
+            x = 0;
+            y = 0;
+            random = new Random();
+        }
+
+        public void Move()
+        {
+            int direction = random.Next(4);
+
+            switch (direction)
+            {
+                case 0:
+                    // Вперед
+                    y++;
+                    Console.WriteLine("Робот {0} двигается вперёд", name);
+                    break;
+                case 1:
+                    // Назад
+                    y--;
+                    Console.WriteLine("Робот {0} двигается назад", name);
+                    break;
+                case 2:
+                    // Поворот налево
+                    x--;
+                    Console.WriteLine("Робот {0} поворачивает налево", name);
+                    break;
+                case 3:
+                    // Поворот направо
+                    x++;
+                    Console.WriteLine("Робот {0} поворачивает направо", name);
+                    break;
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Робот {0} находится на координатах ({1}, {2})", name, x, y);
+        }
+    }
+}
